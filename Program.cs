7a7250723614ddf1e0307025936c1c1bfc67051b
MansiRavindraMohan <manasimohan29@gmail.com>
2023-02-12 13:15:44 +0530

    Getting value on dice

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace SnakeAndLadderProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int dice = rnd.Next(1, 7);
+            Console.WriteLine("Number on the dice is: " +dice);
+          
+        }
+    }
+}
